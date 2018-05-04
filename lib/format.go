09e0154a551540465bc6ed44f9d541140f220693
Masayoshi Mizutani <mizutani@cookpad.com>
2018-05-04 16:26:49 +0900

    Add value counter to Format

--- lib/format.go
+++ lib/format.go
@@ -4,37 +4,11 @@ import (
 // logging "log"
 )
 
+// Format is a structure of log format.
 type Format struct {
-	Chunks []*Chunk `json:"chunk"`
-}
-
-func NewFormat(chunks []*Chunk) *Format {
-	f := Format{}
-	f.Chunks = make([]*Chunk, len(chunks))
-
-	for idx, c := range chunks {
-		f.Chunks[idx] = c.Clone()
-	}
-
-	return &f
-}
-
-func (x *Format) Merge(chunks []*Chunk) {
-	for idx, c := range chunks {
-		if x.Chunks[idx] != nil && x.Chunks[idx].Data != c.Data {
-			x.Chunks[idx] = nil
-		}
-	}
-}
-
-func GenFormat(cluster *Cluster) *Format {
-	format := NewFormat(cluster.Base.Chunk)
-
-	for _, log := range cluster.logs {
-		format.Merge(log.Chunk)
-	}
-
-	return format
+	Chunks []*Chunk
+	values []map[string]int
+	Count  int
 }
 
 func (x Format) String() string {
@@ -48,4 +22,39 @@ func (x Format) String() string {
 	}
 
 	return s
+}
+
+// GenFormat generates a format from Cluster (set of logs)
+func GenFormat(cluster *Cluster) *Format {
+	format := newFormat(cluster.Base.Chunk)
+
+	for _, log := range cluster.logs {
+		format.merge(log.Chunk)
+	}
+
+	return format
+}
+
+func newFormat(chunks []*Chunk) *Format {
+	f := Format{}
+	f.Chunks = make([]*Chunk, len(chunks))
+	f.values = make([]map[string]int, len(chunks))
+
+	for idx, c := range chunks {
+		f.Chunks[idx] = c.Clone()
+		f.values[idx] = map[string]int{}
+	}
+
+	return &f
+}
+
+func (x *Format) merge(chunks []*Chunk) {
+	x.Count++
+	for idx, c := range chunks {
+		if x.Chunks[idx] != nil && x.Chunks[idx].Data != c.Data {
+			x.Chunks[idx] = nil
+		}
+
+		x.values[idx][c.Data]++
+	}
 }
